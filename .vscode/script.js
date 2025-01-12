8959b9fa305754617e51ef8c94f4ed81f6258e9c
Lucas Gabriel <gomesborgesbiel@gmail.com>
2025-01-11 21:46:56 -0300

    Initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,9 @@
+function toggleMode() {
+  const html = document.documentElement
+  // if (html.classList.contains("Light")) {
+  //   html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
+  html.classList.toggle("light")
+}
